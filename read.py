5415ab314b77b9ad27bb03f26d5ba12a16d92a61
Rosie Chen <4everstayhungry@gmail.com>
2022-03-30 22:23:02 +0800

    add basic read.py

---
+++ read.py
@@ -0,0 +1,8 @@
+data = []
+count = 0
+with open ('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1 # count = count + 1
+		if count % 1000 == 0: # %是用來求餘數
+		    print(len(data)) 
